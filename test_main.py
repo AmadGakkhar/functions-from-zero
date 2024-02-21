5f65a1c2e512c783076d03d06b383c61e7bbaad6
Amad Ud Din Gakkhar <51159316+AmadGakkhar@users.noreply.github.com>
2024-02-21 18:44:31 +0000

    Removed Linting Errors

--- test_main.py
+++ test_main.py
@@ -1,10 +1,13 @@
 from utils.funcs import add, diff, wiki_summary
 
+
 def test_add():
-    assert 2 == add(1,1)
+    assert 2 == add(1, 1)
+
 
 def test_diff():
-    assert 3 == diff(2,5)
+    assert 3 == diff(2, 5)
+
 
 def test_wiki_summary():
     assert "Microsoft" in wiki_summary()
